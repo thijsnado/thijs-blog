6000dbbade4d7974a8e16a8a1c6af57619ffc3ec
Thijs de Vries <tdevries.development@gmail.com>
2012-08-25 18:33:25 -0400

    starting to love twitter boostrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post
   include Mongoid::Document
+  include Mongoid::MultiParameterAttributes
+
   field :title, type: String
   field :body, type: String
-  field :tag_list, type: String
+  field :tags, type: Array
   field :published_at, type: Time
+
+  def tag_list
+    Array(tags).join(" ")
+  end
+
+  def body_summary
+    String(body).slice(0..50) + "..."
+  end
 end
